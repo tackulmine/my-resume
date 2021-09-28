a309018a6cb662e26567ea8e264af5053a6ac866
Meftahul Jannah <meftahul.jannah@idntimes.com>
2021-09-28 08:38:23 +0700

    update user profile password hash setter

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,73 +2,78 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-	protected $table = 'users';
-	public static $snakeAttributes = false;
+    protected $table = 'users';
+    public static $snakeAttributes = false;
 
-	protected $dates = [
-		'email_verified_at'
-	];
+    protected $dates = [
+        'email_verified_at',
+    ];
 
-	protected $hidden = [
-		'password',
-		'remember_token'
-	];
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
-	protected $fillable = [
-		'name',
-		'email',
-		'email_verified_at',
-		'password',
-		'remember_token'
-	];
+    protected $fillable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'remember_token',
+    ];
 
-	public function education()
-	{
-		return $this->hasMany(Education::class);
-	}
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
 
-	public function interests()
-	{
-		return $this->hasMany(Interest::class);
-	}
+    public function education()
+    {
+        return $this->hasMany(Education::class);
+    }
 
-	public function languages()
-	{
-		return $this->hasMany(Language::class);
-	}
+    public function interests()
+    {
+        return $this->hasMany(Interest::class);
+    }
 
-	public function projects()
-	{
-		return $this->hasMany(Project::class);
-	}
+    public function languages()
+    {
+        return $this->hasMany(Language::class);
+    }
 
-	public function skills()
-	{
-		return $this->hasMany(Skill::class);
-	}
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 
-	public function socialMedia()
-	{
-		return $this->hasMany(SocialMedia::class);
-	}
+    public function skills()
+    {
+        return $this->hasMany(Skill::class);
+    }
 
-	public function userProfile()
-	{
-		return $this->hasOne(UserProfile::class);
-	}
+    public function socialMedia()
+    {
+        return $this->hasMany(SocialMedia::class);
+    }
 
-	public function workExperiences()
-	{
-		return $this->hasMany(WorkExperience::class);
-	}
+    public function userProfile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
+
+    public function workExperiences()
+    {
+        return $this->hasMany(WorkExperience::class);
+    }
 }
